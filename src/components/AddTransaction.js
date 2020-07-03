c7f9d511cbf91a871874983d577581c48ad1fd76
Pranjal Varshney <pranjal25varshney@gmail.com>
2020-07-04 03:23:04 +0530

    done

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -11,26 +11,29 @@ export const AddTransaction = () => {
     const submitHandle = (e) => {
         e.preventDefault()
 
+       if(!subject || !amount){
+            alert("One or more fields are empty")
+       }else{
         const newtransaction = {
             id: Math.floor(Math.random() * 1000000000),
             subject,
             amount: +amount
         }
-        console.log(newtransaction)
         addTransaction(newtransaction)
+       }
     }
     return (
-        <div>
+        <div className="add-transaction-div">
             <form onSubmit={submitHandle}>
                 <div className="form-control">
-                    <label>Subject</label>
-                    <input type="text" value={subject} onChange={e=>setsubject(e.target.value)} placeholder="enter subject..."/>
+                    <label className="form-label">Subject</label>
+                    <input className="form-input" type="text" value={subject} onChange={e=>setsubject(e.target.value)} placeholder="enter subject..."/>
                 </div>
                 <div className="form-control">
-                    <label>Amount</label>
-                    <input type="number" value={amount} onChange={e=>setamount(e.target.value)} placeholder="enter amount..."/>
+                    <label className="form-label">Amount</label>
+                    <input className="form-input" type="number" value={amount} onChange={e=>setamount(e.target.value)} placeholder="enter amount..."/>
                 </div>
-                <button type="submit">Submit</button>
+                <button className="submit-btn" type="submit">Submit</button>
             </form>
         </div>
     )
